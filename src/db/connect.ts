ca13f6d1eef6432c2f9cd77e77e46cd1b2ef0010
hsiang-cy <hsiang2000.ch@gmail.com>
2025-11-20 16:36:44 +0800

    db test successful

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -3,11 +3,15 @@ import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema.js';
 
+if (!process.env.DB_URL) {
+    throw new Error('DB_URL .env error');
+}
+
 const client = postgres(process.env.DB_URL!, {
     max: 20,
     idle_timeout: 20,
     connect_timeout: 60,
-    ssl: 'require'
+    ssl: false,
 })
 
 export const drizzleORM = drizzle(client, { schema });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
 import userRoute from '#route/user/user.route.ts'
+import { drizzleORM } from '#db/connect.ts'
+import { sql } from 'drizzle-orm'
 
-console.log('App haha.')
+// 測試資料庫連線
+try {
+    await drizzleORM.execute(sql`SELECT 1`)
+    console.log('資料庫連線成功')
+} catch (e) {
+    console.error('資料庫連線失敗：\n', e)
+    process.exit(1)
+}
+
 const app = new Hono()
-
-// middleware
-// app.use('*', prettyJSON())
 
 
 // routes
